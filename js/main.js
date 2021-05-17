03ed10c3aa8315fc070813885ed2f61295ae8f8e
junayedkhan <junayedkhan688@gmail.com>
2021-05-16 20:11:23 -1200

    2 section add

--- js/main.js
+++ js/main.js
@@ -10,43 +10,47 @@ $(document).ready(function () {
   var typed = new Typed('.element', {
     strings: [
       "web designer",
-        "Web developer",
+      "Web developer",
     ],
-      stringsElement: null,
-      typeSpeed: 400,
-      startDelay: 100,
-      backSpeed: 0,
-      smartBackspace: false,
-      backDelay: 700,
-      fadeOut: true,
-      fadeOutClass: 'typed-fade-out',
-      fadeOutDelay: 100,
-      loop: true,
-  
+    stringsElement: null,
+    typeSpeed: 400,
+    startDelay: 100,
+    backSpeed: 0,
+    smartBackspace: false,
+    backDelay: 700,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 100,
+    loop: true,
+
   });
 });
 
 document.addEventListener('mousemove', parallax)
 
-function parallax(e){
+function parallax(e) {
   document.querySelectorAll('.layer').forEach(layer => {
     const speed = layer.getAttribute('data-speed')
-    const x = (window.innerWidth - e.pageX*speed)/100
-    const y = (window.innerHeight - e.pageY*speed)/100
+    const x = (window.innerWidth - e.pageX * speed) / 100
+    const y = (window.innerHeight - e.pageY * speed) / 100
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`
   });
 }
 
-$('.skill-per').each(function(){
+$('.skill-per').each(function () {
   var $this = $(this);
   var per = $this.attr('per');
-  $this.css("width",per+'%');
-  $({animatedValue: 0}).animate({animatedValue: per},{
+  $this.css("width", per + '%');
+  $({
+    animatedValue: 0
+  }).animate({
+    animatedValue: per
+  }, {
     duration: 1000,
-    step: function(){
+    step: function () {
       $this.attr('per', Math.floor(this.animatedValue) + '%');
     },
-    complete: function(){
+    complete: function () {
       $this.attr('per', Math.floor(this.animatedValue) + '%');
     }
   });
@@ -63,16 +67,24 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.list').click(function () {
     const value = $(this).attr('filter_data')
-    if(value == 'all'){
+    if (value == 'all') {
       $('.item').show('1000')
-    }
-    else{
-      $('.item').not('.'+ value).hide('slow')
-      $('.item').filter('.'+ value).show('slow')
+    } else {
+      $('.item').not('.' + value).hide('slow')
+      $('.item').filter('.' + value).show('slow')
     }
   })
-  $('.list').click(function (e) { 
+  $('.list').click(function (e) {
     e.preventDefault();
     $(this).addClass('active').siblings().removeClass('active')
+  });
+});
+
+
+$(document).ready(function () {
+  $('.testimonial_content').owlCarousel({
+    loop: true,
+    items: 1,
+    dots: false
   });
 });
